be95b4cdcd35728439132d78cf54369ca2fb079e
Khizar shah <109973520+KaimonGate@users.noreply.github.com>
2022-08-21 16:05:08 +0530

    Create Palindrome.cpp

---
+++ Practice_Que._Leetcode/Palindrome.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPalindrome(int x) 
+    {
+        string rev=to_string(x);
+        reverse(rev.begin(),rev.end());
+        return rev==to_string(x);
+    }
+};
